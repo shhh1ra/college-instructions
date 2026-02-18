ad3c8f2cecdde96e96fcb6a365ce8f8db5524ad7
shira <85801159+shhh1ra@users.noreply.github.com>
2026-02-18 10:42:00 +0400

    Update isp-automatization.sh

--- isp-automatization.sh
+++ isp-automatization.sh
@@ -20,8 +20,8 @@ $IPT -t nat -A POSTROUTING -s "LAN1_NET" -o "WAN_IF" -j MASQUERADE
 $IPT -t nat -A POSTROUTING -s "LAN2_NET" -o "WAN_IF" -j MASQUERADE
 
 # NAT с локальных сетей на внешку
-iptables -A FORWARD -i ens36 -o ens38 -s 172.16.4.0/28 -j ACCEPT
-iptables -A FORWARD -i ens37 -o ens38 -s 172.16.5.0/28 -j ACCEPT
+$IPT -A FORWARD -i "LAN1_IF" -o "WAN_IF" -s "LAN1_NET" -j ACCEPT
+$IPT -A FORWARD -i "LAN2_IF" -o "WAN_IF" -s "LAN2_NET" -j ACCEPT
 
 # Ответы обратно
 $IPT -A FORWARD -i "WAN_IF" -o "LAN1_IF" -d "LAN1_NET" -m state --state ESTABLISHED,RELATED -j ACCEPT
