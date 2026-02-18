98916b8d672c6fd31c1ad12174d45dd7e8ec73c0
shira <85801159+shhh1ra@users.noreply.github.com>
2026-02-18 11:02:16 +0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+# Создаем systemd unit:
+cat /etc/systemd/system/ip-forward-onboot.service <<'EOF'
+[Unit]
+Description=Enable IPv4 forwarding on boot (safety)
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/sbin/sysctl -w net.ipv4.ip_forward=1
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# перезагружаем службы:
+systemctl daemon-reload
+# включаем и запускаем службу
+systemctl enable --now ip-forward-onboot.service
